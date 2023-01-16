71794187ec7f1466ba72e2cd62736b71e2620890
patrickdemurtas <patrickmurtass@gmail.com>
2023-01-16 01:46:14 -0300

    post participants

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
-import express from "express"
+import express, { request } from "express"
 import cors from "cors"
+import { MongoClient, ObjectId } from "mongodb"
+import dotenv from "dotenv"
+import dayjs from "dayjs"
+import joi from "joi"
+
+dotenv.config()
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+let db;
+
+try {
+    await mongoClient.connect()
+    db = mongoClient.db()
+} catch (error) {
+    console.log('erro no servidor')
+}
 
 const app = express()
 
@@ -7,6 +23,134 @@ app.use(express.json())
 app.use(cors())
 
 const PORT = 5000
+
+
+app.post("/participants", async(request, response) => {
+    const data = request.body
+    const nomeUsuario = request.body.name
+
+    const usuarioSchema = joi.object({
+        name: joi.string().required()
+    })
+
+    const validation = usuarioSchema.validate(data)
+
+    if (validation.error){
+        return response.status(422).send(validation.error.details)
+    }
+
+    try {
+        const checarUsuario = await db.collection("participants").findOne({name: nomeUsuario})
+
+        if(checarUsuario){
+            return response.sendStatus(409)
+        }
+    } catch (error) {
+        response.sendStatus(500)
+    }
+
+    try {
+        await db.collection("participants").insertOne({name: nomeUsuario, lastStatus: Date.now() })
+        await db.collection("messages").insertOne({from: nomeUsuario, to:"Todos", text:"entra na sala...", type:"status", time: dayjs(Date.now()).format("HH:mm:ss")})
+    } catch (error) {
+        response.sendStatus(500)
+    }
+
+})
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => console.log(`servidor rodando na porta: ${PORT}`))
 
