9e0d3628a906ad9efdff902bd5c149163b705d7a
patrickdemurtas <patrickmurtass@gmail.com>
2023-01-16 03:56:08 -0300

    post status

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(cors())
 const PORT = 5000
 
 
-app.post("/participants", async(request, response) => {
+app.post("/participants", async (request, response) => {
     const data = request.body
     const nomeUsuario = request.body.name
 
@@ -35,14 +35,14 @@ app.post("/participants", async(request, response) => {
 
     const validation = usuarioSchema.validate(data)
 
-    if (validation.error){
+    if (validation.error) {
         return response.status(422).send(validation.error.details)
     }
 
     try {
-        const checarUsuario = await db.collection("participants").findOne({name: nomeUsuario})
+        const checarUsuario = await db.collection("participants").findOne({ name: nomeUsuario })
 
-        if(checarUsuario){
+        if (checarUsuario) {
             return response.sendStatus(409)
         }
     } catch (error) {
@@ -50,8 +50,8 @@ app.post("/participants", async(request, response) => {
     }
 
     try {
-        await db.collection("participants").insertOne({name: nomeUsuario, lastStatus: Date.now() })
-        await db.collection("messages").insertOne({from: nomeUsuario, to:"Todos", text:"entra na sala...", type:"status", time: dayjs(Date.now()).format("HH:mm:ss")})
+        await db.collection("participants").insertOne({ name: nomeUsuario, lastStatus: Date.now() })
+        await db.collection("messages").insertOne({ from: nomeUsuario, to: "Todos", text: "entra na sala...", type: "status", time: dayjs(Date.now()).format("HH:mm:ss") })
         return response.sendStatus(201)
     } catch (error) {
         response.sendStatus(500)
@@ -60,7 +60,7 @@ app.post("/participants", async(request, response) => {
 })
 
 
-app.get("/participants", async(request, response) => {
+app.get("/participants", async (request, response) => {
 
     try {
         const usuarios = await db.collection("participants").find().toArray()
@@ -69,25 +69,25 @@ app.get("/participants", async(request, response) => {
         return response.sendStatus(500)
     }
 })
-   
-app.post("/messages", async(request, response) => {
+
+app.post("/messages", async (request, response) => {
     const data = request.body
     const origemUsuario = request.headers.user
 
     const usuarioSchema = joi.object({
         to: joi.string().required(),
         text: joi.string().required(),
-        type: joi.string().valid("message","private_message").required()
+        type: joi.string().valid("message", "private_message").required()
     })
 
     const validation = usuarioSchema.validate(data)
-    if(validation.error){
+    if (validation.error) {
         return response.sendStatus(422)
     }
 
     try {
-        const validarUsuario = await db.collection("participants").findOne({name: origemUsuario})
-        if(!validarUsuario){
+        const validarUsuario = await db.collection("participants").findOne({ name: origemUsuario })
+        if (!validarUsuario) {
             return response.sendStatus(422)
         }
     } catch (error) {
@@ -95,122 +95,51 @@ app.post("/messages", async(request, response) => {
     }
 
     try {
-        await db.collection("messages").insertOne({from: origemUsuario, to: data.to, text: data.text, type: data.type, time: dayjs(Date.now()).format("HH:mm:ss")})
+        await db.collection("messages").insertOne({ from: origemUsuario, to: data.to, text: data.text, type: data.type, time: dayjs(Date.now()).format("HH:mm:ss") })
         response.sendStatus(201)
     } catch (error) {
         response.sendStatus(500)
     }
 
-})  
+})
 
-app.get("/messages", async(request, response) => {
+app.get("/messages", async (request, response) => {
     const { user } = request.headers
     const { query } = request
-   
-  try {
-    const mensagens = await db.collection("messages").find({$or: [{from: user}, {to: user}, {to: "Todos"}]}).toArray()
-    if(query.limit){
-        const limite = Number(query.limit)
-         if(isNaN(limite) || limite < 1){
-            return response.sendStatus(422)
-         } else {
-            return response.send([...mensagens].slice(-limite).reverse())
-         }
-         
+
+    try {
+        const mensagens = await db.collection("messages").find({ $or: [{ from: user }, { to: user }, { to: "Todos" }] }).toArray()
+        if (query.limit) {
+            const limite = Number(query.limit)
+            if (isNaN(limite) || limite < 1) {
+                return response.sendStatus(422)
+            } else {
+                return response.send([...mensagens].slice(-limite).reverse())
+            }
+
+        }
+        return response.send([...mensagens].reverse())
+    } catch (error) {
+        return response.sendStatus(500)
     }
-    return response.send([...mensagens].reverse())
-  } catch (error) {
-    return response.sendStatus(500)
-  }
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.post("/status", async (request, response) => {
+    const { user } = request.headers
+
+    try {
+        const validarUsuario = await db.collection("participants").findOne({ name: user })
+        if (!validarUsuario) {
+            return response.sendStatus(404)
+        } else {
+            await db.collection("participants").updateOne({ name: user }, { $set: { lastStatus: Date.now() } })
+            return response.sendStatus(200)
+        }
+    } catch (error) {
+        return response.sendStatus(500)
+    }
+})
 
 
 
